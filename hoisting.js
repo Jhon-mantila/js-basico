a7d27b26673782e5b59bef596268fa7d996b0d9d
Jhon Mantilla <kaiser_4tu@hotmail.com>
2023-01-12 12:16:51 -0500

    hoisting: eleva:(declara) la variable en el scope global sin inicializar

---
+++ hoisting.js
@@ -0,0 +1,8 @@
+console.log(nombre); //Salida undefined
+var nombre = "jhon";
+
+miFuncion();
+function miFuncion(){
+    console.log("Hola " + nombre);
+}
+var nombre = "jhon"; //Salida Hola undefined

466109c76dc875485f709798920d9e60837a4394
Jhon Mantilla <kaiser_4tu@hotmail.com>
2023-01-11 21:26:21 -0500

    inicio valores con typeof y inicialización de variables

---
+++ variables.js
@@ -0,0 +1,17 @@
+var nombre = "jhon";
+
+//declarar una varibale:
+var edad;
+
+//inicializar la variable
+edad = 35;
+
+edad;
+
+var elementos = ["computador", "celular", "telefono"];
+
+elementos[0];
+var persona = {
+    nombre: "Jhon",
+    edad: 35
+}

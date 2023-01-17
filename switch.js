a0264f3f6857ddfcc61b71a4b6503774e49a856b
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2023-01-16 20:01:27 -0500

    Ejercicio con switch

--- switch.js
+++ switch.js
@@ -13,6 +13,32 @@ switch(numero){
     default:
         console.log("No eres ni 1, 2 ni 3");
     break;
-
-
 }
+
+var article = "computadora";
+
+function solution(article) {
+    
+    switch(article){
+        case "computadora":
+            return "Con mi " + article + " puedo programar usando JavaScript";
+        break;
+
+        case "celular":
+            return "En mi " + article + " puedo aprender usando la app de Platzi";
+        break;
+
+        case "cable":
+            return "¡Hay un " + article + " en mi bota!"
+        break;
+
+        default:
+            return "Artículo no encontrado";
+        break;
+    }
+  }
+
+solution('computadora');
+solution('celular');
+solution('cable');
+solution('ornitorrinco');

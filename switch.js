cbed72458cb6603e79ca2e95e43ddef0eea20ee1
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2023-01-16 19:54:31 -0500

    Creando con el condicional switch

---
+++ switch.js
@@ -0,0 +1,18 @@
+var numero = 1;
+
+switch(numero){
+    case 1:
+        console.log("Soy uno!!");
+    break;
+    case 2:
+        console.log("Soy dos!!");
+    break;
+    case 3:
+        console.log("Soy tres!!");
+    break;
+    default:
+        console.log("No eres ni 1, 2 ni 3");
+    break;
+
+
+}

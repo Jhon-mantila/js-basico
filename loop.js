5b8cc137adb00950c650d93f6a39ade3303a4d3a
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2023-01-16 20:51:07 -0500

    ciclo while

--- loop.js
+++ loop.js
@@ -6,6 +6,22 @@ function saludarEstudiantes(estudiante){
 
 }
 
+//for tradicional
+
 for(var i = 0; i < estudiantes.length ; i++){
     saludarEstudiantes(estudiantes[i]);
+}
+
+//for each
+
+for (var estudiante of estudiantes){
+    saludarEstudiantes(estudiante);
+}
+
+
+while(estudiantes.length > 0){
+
+    var estudiante = estudiantes.shift();
+
+    saludarEstudiantes(estudiante);
 }
